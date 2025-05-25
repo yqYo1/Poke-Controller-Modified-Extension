5983e8465d8b139d7ca9ac6771bb8036d9512dc9
yqYoi <miyasyrog@gmail.com>
2025-05-25 22:47:27 +0900

    fix

--- SerialController/Commands/McuCommands/InfinityWatt.py
+++ SerialController/Commands/McuCommands/InfinityWatt.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from Commands.McuCommandBase import McuCommand
+
+if TYPE_CHECKING:
+    from typing import Final
 
 
 # Mash A button
 class InfinityWatt(McuCommand):
-    NAME = '無限ワット'
+    NAME: Final[str] = "無限ワット"
 
-    def __init__(self, sync_name='inf_watt'):
+    def __init__(self, sync_name: str = "inf_watt") -> None:
         super().__init__(sync_name)

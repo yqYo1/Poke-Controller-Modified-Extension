83d70a9711ef6f6aefe6c3c189d68cfa20284367
yqYoi <miyasyrog@gmail.com>
2025-08-02 23:39:38 +0900

    fix

--- SerialController/Commands/PythonCommands/ImageProcessingOnly/Samples/screenshot.py
+++ SerialController/Commands/PythonCommands/ImageProcessingOnly/Samples/screenshot.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from Commands.Keys import Button, Direction
 from Commands.PythonCommandBase import ImageProcPythonCommand
 
 
 class ScreenShot(ImageProcPythonCommand):
-    NAME = 'スクショ取るだけ'
+    NAME = "スクショ取るだけ"
 
     def __init__(self, cam):
         super().__init__(cam)
@@ -14,5 +10,9 @@ class ScreenShot(ImageProcPythonCommand):
     def do(self):
         self.camera.saveCapture()
         self.camera.saveCapture(filename="ファイル名指定")
-        self.camera.saveCapture(filename="4座標指定", crop=1, crop_ax=[500, 300, 800, 500])
-        self.camera.saveCapture(filename="始点+大きさ指定", crop=2, crop_ax=[500, 300, 300, 300])
+        self.camera.saveCapture(
+            filename="4座標指定", crop=1, crop_ax=[500, 300, 800, 500]
+        )
+        self.camera.saveCapture(
+            filename="始点+大きさ指定", crop=2, crop_ax=[500, 300, 300, 300]
+        )

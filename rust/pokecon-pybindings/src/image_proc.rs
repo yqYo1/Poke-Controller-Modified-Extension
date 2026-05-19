53723bcc15d8d9d1e18f0354d55e7964578cc3f0
yqYoi <52419113+yqYo1@users.noreply.github.com>
2026-05-19 18:33:17 +0900

    fix: restore PyUntypedArrayMethods import in image_proc.rs
    The shape() method requires PyUntypedArrayMethods trait. Removing it
    broke compilation. This is a necessary import, not unused.

--- rust/pokecon-pybindings/src/image_proc.rs
+++ rust/pokecon-pybindings/src/image_proc.rs
@@ -1,4 +1,4 @@
-use numpy::{PyArray3, PyArrayMethods};
+use numpy::{PyArray3, PyArrayMethods, PyUntypedArrayMethods};
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::types::PyTuple;
